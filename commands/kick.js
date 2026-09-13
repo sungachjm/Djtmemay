9bde09ac3d35db79828d1f81d9e04fef76fe48a2
sungachjm <36tinhthanhhoa36@gmail.com>
2026-09-13 11:56:20 +0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,28 +1,28 @@
-const { Command } = require('discord.js'); // Hoặc đổi sang cấu trúc chuẩn của discord.js
 'use strict';
 
 const Command = Loader.require('./core/structures/Command');
 
 class Kick extends Command {
-	constructor(...args) {
-		super(...args);
+    constructor(...args) {
+        super(...args);
 
-		this.aliases      = ['kick'];
-		this.group        = 'Moderator';
-		this.description  = 'Kick a member';
-		this.usage        = 'kick [user] [reason]';
-		this.example      = 'kick @NoobLance Get out!';
-		this.permissions  = 'serverMod';
-		this.requiredPermissions = ['kickMembers'];
-		this.cooldown     = 3000;
-		this.disableDM    = true;
-		this.expectedArgs = 1;
-	}
+        this.aliases           = ['kick'];
+        this.group             = 'Moderator';
+        this.description       = 'Kick a member';
+        this.usage             = 'kick [user] [reason]';
+        this.example           = 'kick @NoobLance Get out!';
+        this.permissions       = 'serverMod';
+        this.requiredPermissions = ['kickMembers'];
+        this.cooldown          = 3000;
+        this.disableDM         = true;
+        this.expectedArgs      = 1;
+    }
 
-	execute({ message, args, guildConfig }) {
-		const Moderation = this.dyno.modules.get('Moderation');
-		const user       = this.resolveUser(message.channel.guild, args[0], null, true);
-		const reason     = args.length > 1 ? args.slice(1).join(' ') : null;
+    execute({ message, args, guildConfig }) {
+        const Moderation = this.dyno.modules.get('Moderation');
+        const user       = this.resolveUser(message.channel.guild, args[0], null, true);
+        const reason     = args.length > 1 ? args.slice(1).join(' ') : null;
+        // ... giữ nguyên phần còn lại của file
 
 		if (!user) {
 			return this.error(message.channel, `I can't find user ${args[0]}.`);
